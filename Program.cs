7b443a26cab7928b398d3f584109b4c0eef52e2b
Hyaku Ju Yon <ichitora114@outlook.com>
2021-03-03 17:29:06 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _3._2Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string MyName;
+
+            MyName = "Hyaku";
+
+            Console.WriteLine(MyName);
+
+            Console.ReadKey();
+        }
+    }
+}
